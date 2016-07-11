4f7a68a695d612d08f0c7788a341aa56e2f328bd
Emily Chen <emily.c.chen@gmail.com>
2016-07-11 13:33:18 -0400

    Update coord

--- js/app.js
+++ js/app.js
@@ -1,26 +1,49 @@
+var latitude;
+var longitude;
+
 angular.module("MyMapApp", [])
 	.controller("MapController", ["$http", function($http) {
 		var vm = this;
+
+		function getWeather() {
+			var url = "https://api.openweathermap.org/data/2.5/weather";
+			var	APIKEY = "3cf58566cfe72e04e12f3a19d54e08dd";
+
+			$http({
+				method: "GET",
+				url: url,
+				APPID: APIKEY,
+				lat: latitude,
+				lon: longitude
+			}).then(function successCallback(response) {
+				console.log("Weather success");
+				console.log(response);
+				vm.description = response.weather.desription;
+			}, function errorCallback(response) {
+				console.log("Weather error");
+				console.log(response);
+			});
+		}
+
+
 		function getMapLocation() {
-			alert("OK");
 			var url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCC6y5Gph7oZCn6n6JVCNYLFxLDUrhnbws";
 
 			$http({
 				url: url,
 				method: "POST"
 			}).then(function successCallback(response){
-				console.log("success!");
+				console.log("geolocation coordinate success!");
 				console.log(response);
 				vm.latitude = response.data.location.lat;
 				vm.longitude = response.data.location.lng;
-				alert(vm.latitude);
-				alert(vm.longitude);
+				latitude = vm.latitude;
+				longitude = vm.longitude;
+				getWeather();
 			}, function errorCallback(response) {
-				console.log("there was an error");
+				console.log("geolocation coordinate error");
 				console.log(response);
 			});
-
-
 		}
 
 		getMapLocation();
