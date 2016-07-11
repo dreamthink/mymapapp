40c085659f469f1002f977f3fa427469fe289d26
Emily Chen <emily.c.chen@gmail.com>
2016-07-11 15:06:24 -0400

    Working Fahrenheit and Celsius toggle

--- js/app.js
+++ js/app.js
@@ -5,31 +5,9 @@ angular.module("MyMapApp", [])
 	.controller("MapController", ["$http", function($http) {
 		var vm = this;
 
-
-		function getWeather() {
-			var url = "https://api.openweathermap.org/data/2.5/weather";
-			var	APIKEY = "3cf58566cfe72e04e12f3a19d54e08dd";
-
-			$http({
-				method: "GET",
-				url: url,
-				APPID: APIKEY,
-				lat: latitude,
-				lon: longitude
-			}).then(function successCallback(response) {
-				console.log("Weather success");
-				console.log(response);
-				vm.description = response.weather.desription;
-			}, function errorCallback(response) {
-				console.log("Weather error");
-				console.log(response);
-			});
-		}
-
-
+	// get geolocation coordinate susing Google Map API to bypass Chrome geolocation restriction to https only
 		function getMapLocation() {
 			var url = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCC6y5Gph7oZCn6n6JVCNYLFxLDUrhnbws";
-
 			$http({
 				url: url,
 				method: "POST"
@@ -40,27 +18,25 @@ angular.module("MyMapApp", [])
 				vm.longitude = response.data.location.lng;
 				latitude = vm.latitude;
 				longitude = vm.longitude;
-				getWeather();
+			// get values in Fahrenheit as default value
+				vm.getWeatherF();
 			}, function errorCallback(response) {
 				console.log("geolocation coordinate error");
 				console.log(response);
 			});
 		}
-
 		getMapLocation();
 
-
-
-
-		function getWeather() {
+	// get values in Fahrenheit
+		 vm.getWeatherF = function() {
 			var url = "http://api.openweathermap.org/data/2.5/weather";
 			var APIKEY = "3cf58566cfe72e04e12f3a19d54e08dd";
 			var params = {
 				APPID: APIKEY,
 				lat: latitude,
-				lon: longitude
+				lon: longitude,
+				units: "imperial"
 			};
-
 			$http({
 				method: "GET",
 				url: url,
@@ -68,14 +44,44 @@ angular.module("MyMapApp", [])
 			}).then(function successCallback(responseWeather) {
 				console.log("Weather success");
 				console.log(responseWeather);
-				console.log(responseWeather.data.main.temp_max);
-				vm.temp = responseWeather.data.main.temp_max;
+				console.log(responseWeather.data.main.temp);
+				vm.temp = responseWeather.data.main.temp;
+				vm.town = responseWeather.data.name;
+				vm.description = responseWeather.data.weather[0].description;
 			}, function errorCallback(responseWeather) {
 				console.log("Weather error");
 				console.log(responseWeather);
 			});
-		}
+		};
 	
+	// get values in Celsius
+		vm.getWeatherC = function() {
+			var url = "http://api.openweathermap.org/data/2.5/weather";
+			var APIKEY = "3cf58566cfe72e04e12f3a19d54e08dd";
+			var params = {
+				APPID: APIKEY,
+				lat: latitude,
+				lon: longitude,
+				units: "metric"
+			};
+			$http({
+				method: "GET",
+				url: url,
+				params: params
+			}).then(function successCallback(responseWeather) {
+				console.log("Weather success");
+				console.log(responseWeather);
+				console.log(responseWeather.data.main.temp);
+				vm.temp = responseWeather.data.main.temp;
+				vm.town = responseWeather.data.name;
+				vm.description = responseWeather.data.weather[0].description;
+				vm.icon = responseWeather.data.weather[0].icon;
+			}, function errorCallback(responseWeather) {
+				console.log("Weather error");
+				console.log(responseWeather);
+			});
+		};
+
 
 
 	}]);
